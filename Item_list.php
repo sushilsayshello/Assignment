2915cde5404dba6dcef0351853cf263036fad2b2
Sushil Shakya <90746527+sushilsayshello@users.noreply.github.com>
2022-10-28 17:23:03 +1100

    Update Item_list.php

--- Item_list.php
+++ Item_list.php
@@ -13,7 +13,15 @@
     </header>
 
     <main>
-
+<!-- Errors -->
+        <?php if (count($errors) > 0) : ?>
+        <h2>Errors</h2>
+        <ul>
+            <?php foreach($errors as $error) : ?>
+            <li><?php echo htmlspecialchars($error); ?></li>
+            <?php endforeach; ?>
+        </ul>
+        <?php endif; ?>
 
 
 <!-- The Items are displayed -->
